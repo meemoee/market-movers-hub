c85c9c7607c0e275e424e3c7d6e7f1b7ea53389e
meemoee <130652582+meemoee@users.noreply.github.com>
2025-01-20 09:25:55 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -51,7 +51,7 @@ export default function Index() {
             <div className="w-[260px]">
               <AccountIsland />
             </div>
-            <div className="flex-1 ml-4">
+            <div className="flex-1 ml-8">
               <TopMoversList
                 topMovers={allMovers}
                 error={error?.message || null}
