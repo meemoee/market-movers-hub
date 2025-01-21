e5fc7b85652d6c066e9e0b700622613a995b77da
meemoee <130652582+meemoee@users.noreply.github.com>
2025-01-20 21:21:48 -0500

    Update TopMoversList.tsx

--- src/components/TopMoversList.tsx
+++ src/components/TopMoversList.tsx
@@ -133,7 +133,7 @@ export default function TopMoversList({
         setIsTimeIntervalDropdownOpen={setIsTimeIntervalDropdownOpen}
       />
       
-      <ScrollArea className="h-[calc(100vh-72px)] w-full px-4 -mt-20">
+      <ScrollArea className="h-[calc(100vh-50px)] w-full px-4 -mt-20">
         <div className="flex flex-col items-center space-y-3 pt-20">
           <MarketStatsBento />
 
