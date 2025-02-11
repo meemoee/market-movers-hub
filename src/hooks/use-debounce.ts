11834c7ed9e2322a43935de86edbe04b6a7e6d21
meemoee <130652582+meemoee@users.noreply.github.com>
2025-02-11 05:22:59 -0500

    Update use-debounce.ts

--- src/hooks/use-debounce.ts
+++ src/hooks/use-debounce.ts
@@ -1,13 +1,12 @@
+import { useState, useEffect } from "react";
 
-import { useEffect, useState } from 'react'
-
-export function useDebounce<T>(value: T, delay?: number): T {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+export default function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500)
-    return () => clearTimeout(timer)
-  }, [value, delay])
+    const handler = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
 
-  return debouncedValue
+  return debouncedValue;
 }
